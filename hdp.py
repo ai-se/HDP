97a365f36104e6d375f23991661f6bd21fe9b97d
WeiFoo <wfu@ncsu.edu>
2015-06-08 18:20:42 -0400

    a little bit tricky here...

--- hdp.py
+++ hdp.py
@@ -2,25 +2,30 @@
 from __future__ import print_function, division
 import sys
 import pdb
+import random
 from os import listdir
 from os.path import isfile, join
+from scipy import stats
+import numpy as np
+
 
 class o:
   def __init__(i, **d): i.__dict__.update(d)
-  # def update(i, **d): i.__dict__.update()
+
   def __getitem__(i, k): return i.__dict__[k]
+
   def __repr__(i):
     keys = [k for k in sorted(i.__dict__.keys()) if k[0] is not "_"]
-    show = [":%s %s"  % (k, i.__dict__[k]) for k in keys]
-    return '{' +' '.join(show) + '}'
+    show = [":%s %s" % (k, i.__dict__[k]) for k in keys]
+    return '{' + ' '.join(show) + '}'
 
 
-def read(src = "./datasetcsv"):
-
+def read(src="./datasetcsv"):
   """
   read data from csv files, return all data in a dictionary
 
-  {'AEEEM':[{attributes=['ck_oo_numberOfPrivateMethods', 'LDHH_lcom', 'LDHH_fanIn'...]
+  {'AEEEM':[{name ='./datasetcsv/SOFTLAB/ar6.csv'
+             attributes=['ck_oo_numberOfPrivateMethods', 'LDHH_lcom', 'LDHH_fanIn'...]
              instances=[[.....],[.....]]},]
    'MORPH':....
    'NASA':....
@@ -29,6 +34,7 @@ def read(src = "./datasetcsv"):
   }
 
   """
+
   def tofloat(lst):
     for x in lst:
       try:
@@ -37,22 +43,40 @@ def read(src = "./datasetcsv"):
         yield x[:-1]
 
   data = {}
-  folders = [ i for i in listdir(src) if not isfile(i) and i != ".DS_Store"]
+  folders = [i for i in listdir(src) if not isfile(i) and i != ".DS_Store"]
   for f in folders:
-    path = join(src,f)
-    for val in [ join(path,i)for i in listdir(path) if i!=".DS_Store"]:
-      d = open(val,"r")
+    path = join(src, f)
+    for val in [join(path, i) for i in listdir(path) if i != ".DS_Store"]:
+      d = open(val, "r")
       content = d.readlines()
       attr = content[0].split(",")
-      inst = [ list(tofloat(row.split(","))) for row in content[1:]]
-      data[f] = data.get(f,[])+[o(name = val ,attr = attr, data =inst)]
-  print(data)
-  pdb.set_trace()
+      inst = [list(tofloat(row.split(","))) for row in content[1:]]
+      data[f] = data.get(f, []) + [o(name=val, attr=attr, data=inst)]
   return data
 
 
+
+
+def KSanalyzer():
+  data = read()
+  pdb.set_trace()
+  for key, val in data.iteritems():
+    for key1, val1 in data.iteritems():
+      if key != key:
+        for one in val:
+          for one1 in val1:
+
+
+
+
+
+
+
+
 if __name__ == "__main__":
-  read()
+  random.seed(1)
+  np.random.seed(1)
+  KSanalyzer()
 
 
 
