3d25d5f6cc4ff74568ace3e5b94de0b20612530e
WeiFoo <wfu@ncsu.edu>
2015-09-28 16:19:40 -0400

    update

--- plots.py
+++ plots.py
@@ -2,7 +2,6 @@ from __future__ import division
 # __author__ = 'WeiFu'
 from matplotlib import pyplot as plt
 import numpy as np
-import sys, pdb
 
 
 def printdic(out):
@@ -27,29 +26,31 @@ def getData(src="./result/0924/epv=10_2_0924_with_SCIPY.txt"):
   return out
 
 
-
-def run(N = 4):
+def run(N=4):
   data = getData()
-  samplesize= [50,100,150,200]
-  x = np.linspace(0,27,28)
-  for plotNum in range(N):
-    onePlot ={}
+  samplesize = [50, 100, 150, 200]
+  x = np.linspace(0, 27, 28)
+  f, (ax1, ax2, ax3, ax4) = plt.subplots(4, sharex=True)
+  ax_lst = [ax1, ax2, ax3, ax4]
+  cols = ['r', 'y', 'g', 'b']
+  for num in range(N):
+    onePlot = {}
     for key, val in data.iteritems():
-      onePlot[key] =[val[0],val[plotNum+1],val[plotNum+1]-val[0]]
-    onePlot_sorted = sorted(onePlot.items(), key = lambda x:x[-1][-1])
-    base = [ i[-1][0] for i in onePlot_sorted]
+      onePlot[key] = [val[0], val[num + 1], val[num + 1] - val[0]]
+    onePlot_sorted = sorted(onePlot.items(), key=lambda x: x[-1][-1])
+    base = [i[-1][0] for i in onePlot_sorted]
     proposed = [i[-1][1] for i in onePlot_sorted]
-    print plotNum
-    # plt.subplot(4,1,plotNum+1)
-    plt.plot(x,base,'ko-',label = 'All')
-    plt.plot(x,proposed,'ro-',label = 'N= '+str(samplesize[plotNum]))
-    plt.legend(fontsize = 'xx-small')
-    plt.xlim([0,28])
+
+    ax_lst[num].plot(x, base, 'ko-', label='All Data')
+    ax_lst[num].plot(x, proposed, 'ro-', label='Sampled with N= ' + str(samplesize[num]), color=cols[num])
+    ax_lst[num].legend(fontsize='small',loc = 0)
+    ax_lst[num].set_xlim([0, 28])
+    ax_lst[num].set_ylim([0, 1])
+    ax_lst[num].set_yticks(np.arange(0.3, 0.9, 0.3))
+  f.subplots_adjust(wspace=0, hspace=0)
+  f.text(0.04, 0.5, 'Improvements', va='center', rotation='vertical', fontsize=11)
   plt.xlabel('Data sets, sorted by improvements')
-  plt.ylabel('Improvements')
   plt.show()
-
-
 
 
 if __name__ == "__main__":
